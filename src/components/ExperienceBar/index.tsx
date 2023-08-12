e6717140a4c49716f52671487b0f5a64b7085143
JaquesBoeno <jaquesboeno@proton.me>
2023-08-12 20:45:09 -0300

    experience bar added

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './styles.scss'
+
+const ExperienceBar: React.FC = () => {
+  const CurrentLevel = 0
+  const CurrentExperience = 0
+  const ExperienceToNextLevel = Math.pow((CurrentLevel + 1) * 4, 2)
+  const CurrentExperiencePercent =
+    (CurrentExperience / ExperienceToNextLevel) * 100
+  return (
+    <div id="ExperienceBar">
+      <span className="current-xp">0xp</span>
+      <div className="bar">
+        <div
+          className="current-xp-bar"
+          style={{ width: `${CurrentExperiencePercent}%` }}
+        ></div>
+        <span
+          className="current-xp-text"
+          style={{ left: `${CurrentExperiencePercent}%` }}
+        >
+          400 xp
+        </span>
+      </div>
+      <span className="next-xp">{ExperienceToNextLevel} xp</span>
+    </div>
+  )
+}
+
+export { ExperienceBar }
--- src/pages/countdown/index.tsx
+++ src/pages/countdown/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import './styles.scss'
+import { ExperienceBar } from '../../components/ExperienceBar'
 
 const Countdown: React.FC = () => {
   return (
     <div id="Countdown">
+      <ExperienceBar />
       <h1>hello worlds</h1>
     </div>
   )
